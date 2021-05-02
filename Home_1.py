1f3efbad8ac5af8c94ab3382692757e854515a6a
Groofi <varchikcreed@mail.ru>
2021-05-03 01:56:15 +0300

    Home_2

--- Home_1.py
+++ Home_1.py
@@ -1,29 +1,57 @@
-# 1. Каждое из слов «разработка», «сокет», «декоратор» представить в строковом формате и проверить тип
-# и содержание соответствующих переменных. Затем с помощью онлайн-конвертера преобразовать строковые представление
-# в формат Unicode и также проверить тип и содержимое переменных.
+# Задание на закрепление знаний по модулю CSV. Написать скрипт, осуществляющий выборку определенных данных из файлов
+# info_1.txt, info_2.txt, info_3.txt и формирующий новый «отчетный» файл в формате CSV. Для этого:
+# Создать функцию get_data(), в которой в цикле осуществляется перебор файлов с данными, их открытие и считывание данных.
+# В этой функции из считанных данных необходимо с помощью регулярных выражений извлечь значения параметров
+# «Изготовитель системы»,  «Название ОС», «Код продукта», «Тип системы». Значения каждого параметра поместить в
+# соответствующий список. Должно получиться четыре списка — например, os_prod_list, os_name_list, os_code_list,
+# os_type_list. В этой же функции создать главный список для хранения данных отчета — например, main_data —
+# и поместить в него названия столбцов отчета в виде списка: «Изготовитель системы», «Название ОС», «Код продукта»,
+# «Тип системы». Значения для этих столбцов также оформить в виде списка и поместить
+# в файл main_data (также для каждого файла);
+import csv
 
-a = "разработка"
-b = "сокет"
-c = "декоратор"
+file_info_all = ["info_1.txt", "info_2.txt", "info_3.txt"]
 
-print(type(a))
-print(a)
+os_prod_list = []
 
-print(type(b))
-print(b)
+os_name_list = []
 
-print(type(c))
-print(c)
+os_code_list = []
 
-a = "%u0440%u0430%u0437%u0440%u0430%u0431%u043E%u0442%u043A%u0430"
-b = "%u0441%u043E%u043A%u0435%u0442"
-c = "%u0434%u0435%u043A%u043E%u0440%u0430%u0442%u043E%u0440"
+os_type_list = []
 
-print(type(a))
-print(a)
 
-print(type(b))
-print(b)
+def get_data():
+    main_data = [["Изготовитель системы", "Название ОС", "Код продукта", "Тип системы"]]
+    for file in file_info_all:
+        with open(f"{file}", encoding="utf-8") as f:
+            read_f = csv.reader(f)
+            for row in read_f:
+                row_value = "".join(row)
+                row_value = row_value.split(":")
+                if main_data[0][0] in row_value:
+                    os_prod_list.append(row_value[1].strip())
+                if main_data[0][1] in row_value:
+                    os_name_list.append(row_value[1].strip())
+                if main_data[0][2] in row_value:
+                    os_code_list.append(row_value[1].strip())
+                if main_data[0][3] in row_value:
+                    os_type_list.append(row_value[1].strip())
+    main_data.append(
+        [
+            os_prod_list,
+            os_name_list,
+            os_code_list,
+            os_type_list
+        ]
+    )
+    return main_data
 
-print(type(c))
-print(c)
+
+def write_csv(file):
+    with open(file, "w") as f:
+        csv_write = csv.writer(f)
+        csv_write.writerows(get_data())
+
+
+write_csv("main_data.csv")
--- Home_2.py
+++ Home_2.py
@@ -1,21 +1,24 @@
-# 2. Каждое из слов «class», «function», «method» записать в байтовом типе без преобразования
-# в последовательность кодов (не используя методы encode и decode) и определить тип,
-# содержимое и длину соответствующих переменных.
-
-a = b"class"
-b = b"function"
-c = b"method"
-
-print(type(a))
-print(len(a))
-print(a)
-
-print(type(b))
-print(len(b))
-print(b)
-
-print(type(c))
-print(len(c))
-print(c)
+# Задание на закрепление знаний по модулю json. Есть файл orders в формате JSON с информацией о заказах.
+# Написать скрипт, автоматизирующий его заполнение данными. Для этого:
+# Создать функцию write_order_to_json(), в которую передается 5 параметров — товар (item), количество (quantity),
+# цена (price), покупатель (buyer), дата (date). Функция должна предусматривать запись данных в виде словаря
+# в файл orders.json. При записи данных указать величину отступа в 4 пробельных символа;
+# Проверить работу программы через вызов функции write_order_to_json() с передачей в нее значений каждого параметра.
+import json
 
 
+def write_order_to_json(item, quantity, price, buyer, date):
+    main_data = {"orders": [
+        {
+            "item": item,
+            "quantity": quantity,
+            "price": price,
+            "buyer": buyer,
+            "date": date,
+        }
+    ]}
+    with open("orders.json", "w") as f:
+        f.write(json.dumps(main_data,indent=4))
+
+
+write_order_to_json("Apple", 10, 100, "Saint-Petersburg", "02.07.2020")
