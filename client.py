2c6e3044dd9ba21e27b45efe018822f07c2a1d49
Groofi <varchikcreed@mail.ru>
2021-05-07 15:26:49 +0300

    Home_3

---
+++ client.py
@@ -0,0 +1,38 @@
+# Реализовать простое клиент-серверное взаимодействие по протоколу JIM (JSON instant messaging):
+# клиент отправляет запрос серверу;
+# сервер отвечает соответствующим кодом результата.
+# Клиент и сервер должны быть реализованы в виде отдельных скриптов, содержащих соответствующие функции.
+# Функции клиента:
+# сформировать presence-сообщение;
+# отправить сообщение серверу;
+# получить ответ сервера;
+# разобрать сообщение сервера;
+# параметры командной строки скрипта client.py <addr> [<port>]:
+# addr — ip-адрес сервера;
+# port — tcp-порт на сервере, по умолчанию 7777.
+import pickle
+import sys
+from socket import socket, AF_INET, SOCK_STREAM
+
+
+def client_start(ip_start="", tcp_start=7777):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect((ip_start, tcp_start))
+    massage = {
+        "Hello": "Привет, сервер",
+        "End": "Готов принять данные"
+    }
+    sock.send(pickle.dumps(massage))
+    data = sock.recv(1024)
+    data_message = pickle.loads(data)
+    print(f'Сообщение: {data_message["Hello"]}. {data_message["End"]}.')
+    sock.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        client_start()
+    else:
+        argv_1 = sys.argv[1]
+        argv_2 = int(sys.argv[2][1:5])
+        client_start(argv_1, argv_2)
---
+++ server.py
@@ -0,0 +1,50 @@
+# Реализовать простое клиент-серверное взаимодействие по протоколу JIM (JSON instant messaging):
+# клиент отправляет запрос серверу;
+# сервер отвечает соответствующим кодом результата.
+# Клиент и сервер должны быть реализованы в виде отдельных скриптов, содержащих соответствующие функции.
+# Функции сервера:
+# принимает сообщение клиента;
+# формирует ответ клиенту;
+# отправляет ответ клиенту;
+# имеет параметры командной строки:
+# -p <port> — TCP-порт для работы (по умолчанию использует 7777);
+# -a <addr> — IP-адрес для прослушивания (по умолчанию слушает все доступные адреса).
+import pickle
+import sys
+from socket import socket, AF_INET, SOCK_STREAM
+
+
+def server_start(ip_start="", tcp_start=7777):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind((ip_start, tcp_start))
+    sock.listen(5)
+
+    while True:
+        client, address = sock.accept()
+        data = client.recv(1024)
+        data_message = pickle.loads(data)
+        print(f'Сообщение: {data_message["Hello"]}. {data_message["End"]}.\nБыло отправлено клиентом: {address}.')
+        massage = {
+            "Hello": "Привет, клиент",
+            "End": f"Ваши данные длинной в {len(data)} байт"
+        }
+        client.send(pickle.dumps(massage))
+        client.close()
+
+
+if __name__ == "__main__":
+    print(len(sys.argv))
+    if len(sys.argv) <= 1:
+        server_start()
+    elif len(sys.argv) >= 4:
+        argv_1 = sys.argv[4]
+        argv_2 = int(sys.argv[2])
+        server_start(argv_1, argv_2)
+    else:
+        for param in sys.argv:
+            if param == "-p":
+                argv_1 = int(sys.argv[2])
+                server_start(tcp_start=argv_1)
+            elif param == "-a":
+                argv_1 = sys.argv[2]
+                server_start(ip_start=argv_1)
