cb965237994e2bc0e1b80e7439c901f48138836e
Groofi <varchikcreed@mail.ru>
2021-05-18 23:05:42 +0300

    Home_4.8

--- server.py
+++ server.py
@@ -14,24 +14,38 @@ import sys
 from socket import socket, AF_INET, SOCK_STREAM
 
 
-def server_start(ip_start="", tcp_start=7777):
+def server_to_accept(sock_cli):
+    client, address = sock_cli.accept()
+    return client, address
+
+
+def server_to_accept_message(sock):
+    data = sock.recv(1024)
+    data_message = pickle.loads(data)
+    return f'Сообщение от клиента {sock.getpeername()}: {data_message}'
+
+
+def server_connect(ip_start="", tcp_start=7777):
     sock = socket(AF_INET, SOCK_STREAM)
     sock.bind((ip_start, tcp_start))
-    sock.listen(1)
+    sock.listen(5)
+    return sock
 
+
+def server_send(cli_add, message):
+    cli_add.send(pickle.dumps(message))
+
+
+def server_start(ip_go="", tcp_go=7777):
+    sock = server_connect(ip_go, tcp_go)
     while True:
-        client, address = sock.accept()
-        data = client.recv(1024)
-        data_message = pickle.loads(data)
-        print(f'Сообщение: {data_message["message"]}')
-        massage = {
-            "message": "Привет, клиент",
-        }
-        client.send(pickle.dumps(massage))
+        client, address = server_to_accept(sock)
+        message = server_to_accept_message(client)
+        server_send(client, message)
         client.close()
 
 
-if __name__ == "__main__":
+def start_script():
     if len(sys.argv) <= 1:
         server_start()
     elif len(sys.argv) >= 4:
@@ -42,7 +56,11 @@ if __name__ == "__main__":
         for param in sys.argv:
             if param == "-p":
                 argv_1 = int(sys.argv[2])
-                server_start(tcp_start=argv_1)
+                server_start(tcp_go=argv_1)
             elif param == "-a":
                 argv_1 = sys.argv[2]
-                server_start(ip_start=argv_1)
+                server_start(ip_go=argv_1)
+
+
+if __name__ == "__main__":
+    start_script()
