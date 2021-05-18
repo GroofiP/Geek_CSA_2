a238191fb2db482244cb7dc26b0079a141fa4e6b
Groofi <varchikcreed@mail.ru>
2021-05-18 12:04:38 +0300

    Home_6.3

--- main.py
+++
@@ -1,6 +0,0 @@
-from log.server_log_config import logger
-
-
-def main(func_ser_cli):
-    logger.info(f"Функция {func_ser_cli.__name__} вызвана из функции {main.__name__}")
-    func_ser_cli("127.0.0.1", 7777)
